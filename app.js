768342b7e6444daf6bbb6eda87d89a5c94db33f2
DylanBoucher <dylanboucher99@gmail.com>
2022-04-06 16:37:29 -0500

    starting in the javascript and making hit and miss classes

---
+++ app.js
@@ -0,0 +1,10 @@
+const ships = ['Carrier', 'Battleship', 'Cruiser', 'Submarine', 'Destroyer']
+console.log(ships)
+
+
+const cell = document.querySelectorAll('td')
+console.log(cell)
+
+// cell.addEventListenter('click', () => {
+//     cell.classList.add('hit')
+// })
